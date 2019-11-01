5db6f7b558f15ab7fca2f716b3fbe5be2b527d75
lmbaeza <lmbaeza@unal.edu.co>
2019-11-01 15:45:20 -0500

    Se agregaron algunos comentarios por corregir en el Modelo 'ResidencePublication'

--- Apps/StudentHousing/models.py
+++ Apps/StudentHousing/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from Apps.Users.models import People
 
 class ResidencePublication(models.Model):
     name = models.CharField(max_length=255)
@@ -6,24 +7,30 @@ class ResidencePublication(models.Model):
     price = models.IntegerField()
     address = models.CharField(max_length=64)
     rules = models.CharField(max_length=1024)
+
+    # TODO: Cuando Vladimir haya creado el modelo 'neighborhood' y 'locality'
+    # de la App 'Location' Hacer las relaciones
     neighborhood = models.CharField(max_length=255)
     locality = models.CharField(max_length=16)
-    landlord = models.CharField(max_length=255)
+
+    # Hacer la relación ForeignKey con Owner de la App Users
+    owner = models.CharField(max_length=255)
 
 
-from Apps.Users.models import People
 class Comment(models.Model):
     content = models.CharField(max_length=255)
     publication = models.ForeignKey(ResidencePublication, null=False, blank=False, on_delete=models.CASCADE)
     person = models.ForeignKey(People, null=False, blank=False, on_delete=models.CASCADE)
 
-from Apps.Users.models import People
+
 class Qualification(models.Model):
     number = models.IntegerField()
     publication = models.ForeignKey(ResidencePublication, null=False, blank=False, on_delete=models.CASCADE)
     person = models.ForeignKey(People, null=False, blank=False, on_delete=models.CASCADE)
 
+
 class Service(models.Model):
-    number = models.IntegerField()
-    description = rules = models.CharField(max_length=1024)
+    name = models.CharField(max_length=1024)
+    description = models.CharField(max_length=1024)
     publication = models.ForeignKey(ResidencePublication, null=False, blank=False, on_delete=models.CASCADE)
+
