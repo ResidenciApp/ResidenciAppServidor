4684582b549dcb5951417c7880f96aced49f3575
Software <gdgarcian@unal.edu.co>
2019-11-03 03:41:04 +0100

    Descripcion del commit

--- Apps/StudentHousing/models.py
+++ Apps/StudentHousing/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from Apps.Users.models import Owner
 class ResidencePublication(models.Model):
     name = models.CharField(max_length=255)
     photo = models.CharField(max_length=255)
@@ -8,7 +9,7 @@ class ResidencePublication(models.Model):
     rules = models.CharField(max_length=1024)
     neighborhood = models.CharField(max_length=255)
     locality = models.CharField(max_length=16)
-    landlord = models.CharField(max_length=255)
+    owner = models.ForeignKey(Owner, null=False, blank=False, on_delete=models.CASCADE)
 
 
 from Apps.Users.models import People
