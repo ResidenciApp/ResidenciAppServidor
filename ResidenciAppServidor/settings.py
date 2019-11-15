86b50aa077ee282cde8429c3a6359f361df29cdc
lmbaeza <lmbaeza@unal.edu.co>
2019-11-14 23:05:19 -0500

    Se agregó la configuración de la DB en Heroku

--- ResidenciAppServidor/settings.py
+++ ResidenciAppServidor/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -90,6 +91,9 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=20)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
